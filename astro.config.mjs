51d2873c0b75b9a2e208195740cb3d54178807d3
ChellappanRajan <vrchellappan@gmail.com>
2025-02-17 22:20:28 +0530

    feat: add GitHub Actions workflow for deployment to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://chellappanrajan.github.io/',
+  base: 'thoughts-and-tech',
   integrations: [tailwind()]
 });
